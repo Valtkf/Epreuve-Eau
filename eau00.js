42fd9ae76a92ac0a8b39751ce674721efc3ba5e8
Valentin Gregorio <110051124+Valtkf@users.noreply.github.com>
2023-05-25 22:16:44 +0200

    Create eau00.js

---
+++ eau00.js
@@ -0,0 +1,7 @@
+//Créez un programme qui affiche toutes les différentes combinaisons possibles de trois chiffres dans l'odre croissant, dans l'ordre croissant. La répétition est volontaire.
+
+//Exemples d'utilisation :
+//012, 013, 014, 015, 016, 017, 018, 019, 023, 024, ... , 789
+// 987 n'est pas là parce que 789 est présent
+//020 n'est pas là parce que 0 apparaît deux fois
+//000 n'est pas à parce que cette combinaison ne comporte pas exclusivement des chiffres différents les uns des autres.
