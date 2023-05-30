0b16042b1242d7c89b2a6351645f01ad148dafaa
Valentin Gregorio <110051124+Valtkf@users.noreply.github.com>
2023-05-30 18:01:27 +0200

    Update eau00.js

--- eau00.js
+++ eau00.js
@@ -4,20 +4,21 @@
 //012, 013, 014, 015, 016, 017, 018, 019, 023, 024, ... , 789
 // 987 n'est pas là parce que 789 est présent
 //020 n'est pas là parce que 0 apparaît deux fois
-//000 n'est pas à parce que cette combinaison ne comporte pas exclusivement des chiffres différents les uns des autres.
+//000 n'est pas là parce que cette combinaison ne comporte pas exclusivement des chiffres différents les uns des autres.
 
-if (chiffre1 !== chiffre2 && chiffre1 !== chiffre3 && chiffre2 !== chiffre3) {
-    // Afficher la combinaison de trois chiffres (i, j, k) dans l'ordre croissant 
-  } 
-
-
-for (let chiffre1 = 0; chiffre1 < 10; chiffre1++) {
-    for (let chiffre2 = 0; chiffre2 < 10; chiffre2++) {
-        for (let chiffre3 = 0; chiffre3 < 10; chiffre3++) {
-            console.log(chiffre1 , chiffre2 , chiffre3);
+for (let i = 0; i <= 9; i++) {
+    for (let j = i; j <= 9; j++) {
+      for (let k = j; k <= 9; k++) {
+        if (i === j && j === k) {
+          // Les trois chiffres sont identiques, ignore cette combinaison
+          continue;
+        } else if (i === j || j === k) {
+          // L'un des chiffres se répète, ignore cette combinaison
+          continue;
         }
+        
+        // Afficher la combinaison de trois chiffres (i, j, k) dans l'ordre croissant
+        console.log("" + i + j + k);
+      }
     }
-    
-}
-
-
+  }
