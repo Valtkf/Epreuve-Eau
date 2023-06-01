54672b2eb54e4159875bc337a6fd49e4120b369b
Valentin Gregorio <110051124+Valtkf@users.noreply.github.com>
2023-06-01 16:41:10 +0200

    Create eau02.js

---
+++ eau02.js
@@ -0,0 +1,15 @@
+/*
+Créez un programme qui affiche ses arguments reçus à l’envers.
+Exemples d’utilisation :
+$> python exo.py “Suis” “Je” “Drôle”
+Drôle
+Je
+Suis
+$> python exo.py ha ho
+ho
+ha
+$> python exo.py “Bonjour 36”
+Bonjour 36
+
+Afficher error et quitter le programme en cas de problèmes d’arguments.
+*/
