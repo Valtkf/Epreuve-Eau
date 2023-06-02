eaecaf1c042277d77dae63933286dcb4f35cf635
Valentin Gregorio <110051124+Valtkf@users.noreply.github.com>
2023-06-02 23:46:46 +0200

    Update eau02.js

--- eau02.js
+++ eau02.js
@@ -13,3 +13,11 @@ Bonjour 36
 
 Afficher error et quitter le programme en cas de problèmes d’arguments.
 */
+const argument = process.argv[2];
+const arg = process.spplice[2];
+
+let reverseArg =()=> {
+    for (let i = arg.length; i>=l; i-- ) {
+        console.log(arg[i-l])
+    }
+}
