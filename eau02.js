a48725fcdb1c8a3b9b987b5a9631205a0677585f
Valentin Gregorio <110051124+Valtkf@users.noreply.github.com>
2023-06-05 17:14:34 +0200

    Update eau02.js

--- eau02.js
+++ eau02.js
@@ -13,19 +13,23 @@ Bonjour 36
 
 Afficher error et quitter le programme en cas de problèmes d’arguments.
 */
+
+// variables
 const argument = process.argv[2];
 const arg = process.argv.splice(2);
 
-
+// fonction pour inverser la chaîne de caractère
 let reverse =()=> {
     for (let i = arg.length; i>=1; i-- ) {
         console.log(arg[i-1]);
     }
 }
 
+// gestion des erreurs
 if (!argument) {
     console.log ("Veuillez rentrer des arguments");
     process.exit()
 }
 
+// appel de la fonction
 reverse(arg)
