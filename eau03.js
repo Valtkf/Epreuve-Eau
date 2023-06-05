1c962aeeb433f9322acb7fbbab8acc8a297a3a63
Valentin Gregorio <110051124+Valtkf@users.noreply.github.com>
2023-06-05 17:50:38 +0200

    Update eau03.js

--- eau03.js
+++ eau03.js
@@ -15,6 +15,10 @@ const fibo = new Array(n);
 fibo[0] = 0
 fibo[1] = 1
 
+if (n < 0 || isNaN) {
+  console.log("erreur");
+}
+
 calc = ()=> {
   for (let i = 2; i < n; i = i + 1) {
     fibo[i] = fibo[i - 1] + fibo[i - 2]
