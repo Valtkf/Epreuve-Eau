85b493f87d0805f44efedf179b49a52a380bee33
Valentin Gregorio <110051124+Valtkf@users.noreply.github.com>
2023-06-07 16:42:50 +0200

    Update eau04.js

--- eau04.js
+++ eau04.js
@@ -1,1 +1,10 @@
+/* Créez un programme qui affiche le premier nombre premier supérieur aunombre donné en argument
+
+Exemples d'utilisation :
+$> python exo.py 14
+17
+$>
+
+Afficher -1 si le paramètre est négatif oumauvais
+*/
 
