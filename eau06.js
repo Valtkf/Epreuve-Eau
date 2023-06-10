0a7d1236b9e137936b423843351aa7209eb2d990
Valentin Gregorio <110051124+Valtkf@users.noreply.github.com>
2023-06-10 16:31:52 +0200

    Update eau06.js

--- eau06.js
+++ eau06.js
@@ -1,1 +1,41 @@
+/*Créez un programme qui met en majuscule une lettre sur deux d’une chaîne de caractères. Seule les lettres (A-Z, a-z) sont prises en compte.
+
+
+Exemples d’utilisation :
+$> python exo.py “Hello world !”
+HeLlO wOrLd !
+
+
+$> python exo.py 42
+error
+
+Afficher error et quitter le programme en cas de problèmes d’arguments.
+*/
+
+const capitalLetter = () => {
+   const str = process.argv[2];
+   let count = 0;
+   let result = "";
+
+   for (let i=0; i<str.length; i++) {
+    const character = str.charAt(i);
+
+    if(/[a-zA-Z]/.test(character)) {
+        let modifiedLetter = count % 2 === 0 ? character.toUpperCase() : character.toLowerCase();
+        result += modifiedLetter;
+        count++;
+    } else {
+        result += character;
+    }
+   }
+
+
+   return result;
+
+   
+
+}
+
+const finalResult = capitalLetter();
+console.log(finalResult);
 
