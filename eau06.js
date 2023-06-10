73483f779c3428361fd6549e104381c56db39ac5
Valentin Gregorio <110051124+Valtkf@users.noreply.github.com>
2023-06-10 17:54:32 +0200

    Update eau06.js

--- eau06.js
+++ eau06.js
@@ -13,13 +13,14 @@ Afficher error et quitter le programme en cas de problèmes d’arguments.
 */
 
 const capitalLetter = () => {
-   const str = process.argv[2];
+   const str = process.argv.slice(2).join(" ");
    let count = 0;
    let result = "";
 
    for (let i=0; i<str.length; i++) {
     const character = str.charAt(i);
 
+    
     if(/[a-zA-Z]/.test(character)) {
         let modifiedLetter = count % 2 === 0 ? character.toUpperCase() : character.toLowerCase();
         result += modifiedLetter;
@@ -28,13 +29,22 @@ const capitalLetter = () => {
         result += character;
     }
    }
-
-
    return result;
-
-   
-
 }
+
+
+const handleErrors = () => {
+if (process.argv.length < 3) {
+    console.log("Veuillez fournir une chaîne de caractères en argument.");
+    process.exit(1);
+  }
+  if (!isNaN(process.argv[2])) {
+    console.log("Erreur : La chaîne de caractères ne peut pas être un nombre.");
+    process.exit(1);
+ }
+};
+
+handleErrors();
 
 const finalResult = capitalLetter();
 console.log(finalResult);
