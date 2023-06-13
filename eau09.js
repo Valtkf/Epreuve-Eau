bed14c07b3fda6196bbfc0c6db1ef6f0751f13ef
Valentin Gregorio <110051124+Valtkf@users.noreply.github.com>
2023-06-13 22:29:37 +0200

    Create eau09.js

---
+++ eau09.js
@@ -0,0 +1,15 @@
+/*Créez un programme qui affiche toutes les valeurs comprises entre deux nombres dans l’ordre croissant. Min inclus, max exclus.
+
+
+Exemples d’utilisation :
+$> python exo.py 20 25
+20 21 22 23 24
+
+
+$> python exo.py 25 20
+20 21 22 23 24
+
+$> python exo.py hello
+error
+
+Afficher error et quitter le programme en cas de problèmes d’arguments.*/
