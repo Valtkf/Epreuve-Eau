3a5deba8e145e342aef81eb2d52ed38157373666
Valentin Gregorio <110051124+Valtkf@users.noreply.github.com>
2023-06-13 22:41:39 +0200

    Update eau09.js

--- eau09.js
+++ eau09.js
@@ -13,3 +13,28 @@ $> python exo.py hello
 error
 
 Afficher error et quitter le programme en cas de problèmes d’arguments.*/
+
+const displayNumbers = (min, max) => {
+    // Vérification de la validité des arguments
+    if (isNaN(min) || isNaN(max)) {
+      console.error("Erreur : les arguments doivent être des nombres valides.");
+      return;
+    }
+  
+    // Vérification de l'ordre des nombres
+    const [start, end] = min <= max ? [min, max] : [max, min];
+  
+    // Affichage des nombres dans l'ordre croissant
+    for (let i = start; i < end; i++) {
+      console.log(i);
+    }
+  };
+  
+  // Vérification des arguments
+  if (process.argv.length < 4) {
+    console.error("Erreur : veuillez fournir deux nombres en arguments.");
+  } else {
+    const min = parseInt(process.argv[2]);
+    const max = parseInt(process.argv[3]);
+    displayNumbers(min, max);
+  }
