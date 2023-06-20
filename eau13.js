9d87efc98e08ad594829d7464f39f940e887e2b7
Valtkf <valentin.vgregorio@gmail.com>
2023-06-20 22:44:25 +0200

    eau13

---
+++ eau13.js
@@ -0,0 +1,29 @@
+/* Créez un programme qui trie une liste de nombres. Votre programme devra implémenter l’algorithme du tri par sélection.
+
+Vous utiliserez une fonction de cette forme (selon votre langage) :
+my_select_sort(array) {
+	# votre algorithme
+	return (new_array)
+}
+
+
+Exemples d’utilisation :
+$> python exo.py 6 5 4 3 2 1
+1 2 3 4 5 6
+
+
+$> python exo.py test test test
+error
+
+Afficher error et quitter le programme en cas de problèmes d’arguments.
+
+
+Wikipedia vous présentera une belle description de cet algorithme de tri. */
+const selectionSort = (array) => {
+    for (let i = 0; i < array.length -1; i++)
+    let indexMin = i;
+};
+
+
+
+const array = process.argv.slice(2);
