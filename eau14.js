53a1810cfe61524a011047c60dd9d6bc8022a09a
Valtkf <valentin.vgregorio@gmail.com>
2023-06-23 11:54:24 +0200

    eau14 début

---
+++ eau14.js
@@ -0,0 +1,34 @@
+/*Créez un programme qui trie les éléments donnés en argument par ordre ASCII.
+
+
+Exemples d’utilisation :
+$> python exo.py Alfred Momo Gilbert
+Alfred Gilbert Momo
+
+
+$> python exo.py A Z E R T Y
+A E R T Y Z
+
+Afficher error et quitter le programme en cas de problèmes d’arguments.*/
+
+function insertionSort(array) {
+  const length = array.length;
+
+  for (let i = 1; i < length; i++) {
+    let current = array[i];
+    let j = i - 1;
+
+    while (j >= 0 && array[j] > current) {
+      array[j + 1] = array[j];
+      j--;
+    }
+
+    array[j + 1] = current;
+  }
+
+  return array;
+}
+
+const array = process.argv.slice(2);
+const sortedArray = insertionSort(array);
+console.log(sortedArray);
